ae66aebc8a12afc29566876c589104e1ba5fc413
Fuyuxuan <jace.fu@icloud.com>
2023-08-29 16:45:43 +0800

    update

--- code/index.py
+++ code/index.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
 import json
+from towhee import pipe
+
 
 # To enable the initializer feature (https://help.aliyun.com/document_detail/158208.html)
 # please implement the initializer function as below：
@@ -11,5 +13,12 @@ import json
 def handler(event, context):
   # evt = json.loads(event)
   logger = logging.getLogger()
-  logger.info('hello world')
+  # logger.info('hello world')
+  p = pipe.input('num').output('num')
+  res = p(1)
+  print(res.to_list())
   return '200'
+
+# p = pipe.input('num').output('num')
+# res = p(1)
+# print(res.to_list())
